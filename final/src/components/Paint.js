66062e73896e3ec567c5ef6213d4953c4f504cf7
Michael Snowden <mtsnowden@gmail.com>
2019-07-24 15:35:22 -0500

    working

--- final/src/components/Paint.js
+++ final/src/components/Paint.js
@@ -5,6 +5,10 @@ export default function Paint() {
         <div>
             <h1>Welcome to the Painting page</h1>
             <p>Here I'll have pictures of paintings that I've done</p>
+            <p>im thinking either grid style or a single
+                image feed like instagram. Maybe have an option for 
+                visitors to comment on image or purchase the piece.
+            </p>
         </div>
     )
 }
