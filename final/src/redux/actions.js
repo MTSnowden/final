48160d4e484a8e159a228e68b2b7b9d014aa3690
Michael Snowden <mtsnowden@gmail.com>
2019-07-30 15:09:53 -0500

    working on authentication

--- final/src/redux/actions.js
+++ final/src/redux/actions.js
@@ -1,13 +1,82 @@
-export function login(user) {
-    return {
-      type: 'LOGIN',
-      value: user
-    }
+import axios from 'axios'
+import jwt from 'jsonwebtoken'
+
+export function login({ userName, password }) {
+  return dispatch => {
+    return axios({
+      url: '/auth/login',
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: JSON.stringify({ userName, password })
+    })
+    .then(res => {
+      document.cookie = `id_token=${res.data};max-age=300;`
+      const payload = jwt.verify(res.data, 'secret')
+      dispatch({
+        type: 'LOGIN',
+        value: payload._doc
+      })
+    })
+    .catch(err => Promise.reject(err))
   }
-  
-  export function logout(user) {
-    return {
-      type: 'LOGOUT',
-      value: user
-    }
+}
+
+export function logout() {
+  return {
+    type: 'LOGOUT'
   }
+}
+
+export function signUp({ userName, password }) {
+  return dispatch => {
+    return axios({
+      url: '/auth/signup',
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: JSON.stringify({ userName, password })
+    })
+    .then((res) => dispatch(login({ userName, password })))
+    .catch(err => Promise.reject(err))
+  }
+}
+
+export function setUser(user) {
+  return {
+    type: 'SET_USER',
+    value: user
+  }
+}
+
+export function updateUser(userName) {
+  return dispatch => {
+    return axios({
+      url: '/auth/user',
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: JSON.stringify({ userName }),
+      withCredentials: true
+    })
+    .catch(err => Promise.reject(err))
+  }  
+}
+
+export function updatePassword(password) {
+  return dispatch => {
+    return axios({
+      url: '/auth/password',
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: JSON.stringify({ password }),
+      withCredentials: true
+    })
+    .catch(err => Promise.reject(err))
+  }  
+}
