34f7bb4448590ed11971d99705d54adb22b39c45
Michael Snowden <mtsnowden@gmail.com>
2019-08-10 06:16:15 -0500

    added images

--- final/src/components/Draw.js
+++ final/src/components/Draw.js
@@ -4,15 +4,13 @@ import draw1 from '../final-img/draw1.jpg'
 import draw1Detail from '../final-img/draw1Detail.png'
 import draw2 from '../final-img/draw2.jpg'
 import draw3 from '../final-img/draw3.jpg'
-import wasp from '../final-img/wasp.jpg'
-
+import figure1 from '../final-img/figure1.png'
+import figure2 from '../final-img/figure2.png'
+import figure3 from '../final-img/figure3.png'
+import figure4 from '../final-img/figure4.png'
+import figure5 from '../final-img/figure5.png'
 
 const photos = [{
-    photo: wasp,
-    caption: "Embrace the pain",
-    subcaption: "Charcoal on paper - 2017",
-    thumbnail: wasp,
-  },{
     photo:  draw1,
     caption: "Impending Doom",
     subcaption: "Charcoal on paper - 2012",
@@ -32,6 +30,31 @@ const photos = [{
     caption: "Interior",
     subcaption: "Charcoal on paper - 2012",
     thumbnail: draw3,
+  }, {
+    photo: figure1,
+    caption: "Nude",
+    subcaption: "Charcoal on paper - 2016",
+    thumbnail: figure1,
+  }, {
+    photo: figure2,
+    caption: "Nude",
+    subcaption: "Charcoal on paper - 2014",
+    thumbnail: figure2,
+  }, {
+    photo: figure3,
+    caption: "Nude",
+    subcaption: "Charcoal on paper - 2017",
+    thumbnail: figure3,
+  }, {
+    photo: figure4,
+    caption: "Nude",
+    subcaption: "Charcoal on paper - 2015",
+    thumbnail: figure4,
+  }, {
+    photo: figure5,
+    caption: "Nude",
+    subcaption: "Charcoal on paper - 2018",
+    thumbnail: figure5,
   }]
 
 export default class DrawGallery extends Component {
